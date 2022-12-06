4f81a491b7fcea71d6ada6d9ea5707d06dc899fe
chen <569922982@qq.com>
2022-12-06 11:26:21 +0800

    up

--- sword/resource/assets_list.go
+++ sword/resource/assets_list.go
@@ -1,7 +1,7 @@
 package resource
 
 var AssetsList = []string{
-	"/dist/css/all.min.782fc50d44.css",
+	"/dist/css/all.min.79250b7051.css",
 	"/dist/css/blue.png",
 	"/dist/css/blue@2x.png",
 	"/dist/css/fonts/6xK3dSBYKcSV-LCoeQqfX1RYOo3qOK7g.ttf",
@@ -36,12 +36,12 @@ var AssetsList = []string{
 	"/dist/img/ui-icons_777777_256x240.png",
 	"/dist/img/ui-icons_cc0000_256x240.png",
 	"/dist/img/ui-icons_ffffff_256x240.png",
-	"/dist/js/all.min.336acc9b5c.js",
-	"/dist/js/all_2.min.e20dec6087.js",
-	"/dist/js/datatable.min.15c62afef0.js",
-	"/dist/js/form.min.d2b241b5dc.js",
+	"/dist/js/all.min.8425540791.js",
+	"/dist/js/all_2.min.38a2a946b0.js",
+	"/dist/js/datatable.min.581cdc109b.js",
+	"/dist/js/form.min.f8678914e9.js",
 	"/dist/js/html5shiv.min.js",
 	"/dist/js/respond.min.js",
-	"/dist/js/tree.min.372157bb0a.js",
-	"/dist/js/treeview.min.e1c09ea5a3.js",
+	"/dist/js/tree.min.e1faf8b7de.js",
+	"/dist/js/treeview.min.7780d3bb0f.js",
 }
--- sword/resource/assets_path.go
+++ sword/resource/assets_path.go
@@ -1,13 +1,13 @@
 package resource
 
 var AssetPaths = map[string]string{
-	"all.min.css":      "/dist/css/all.min.782fc50d44.css",
-	"all.min.js":       "/dist/js/all.min.336acc9b5c.js",
-	"all_2.min.js":     "/dist/js/all_2.min.e20dec6087.js",
-	"datatable.min.js": "/dist/js/datatable.min.15c62afef0.js",
-	"form.min.js":      "/dist/js/form.min.d2b241b5dc.js",
+	"all.min.css":      "/dist/css/all.min.79250b7051.css",
+	"all.min.js":       "/dist/js/all.min.8425540791.js",
+	"all_2.min.js":     "/dist/js/all_2.min.38a2a946b0.js",
+	"datatable.min.js": "/dist/js/datatable.min.581cdc109b.js",
+	"form.min.js":      "/dist/js/form.min.f8678914e9.js",
 	"html5shiv.min.js": "/dist/js/html5shiv.min.js",
 	"respond.min.js":   "/dist/js/respond.min.js",
-	"tree.min.js":      "/dist/js/tree.min.372157bb0a.js",
-	"treeview.min.js":  "/dist/js/treeview.min.e1c09ea5a3.js",
+	"tree.min.js":      "/dist/js/tree.min.e1faf8b7de.js",
+	"treeview.min.js":  "/dist/js/treeview.min.7780d3bb0f.js",
 }
